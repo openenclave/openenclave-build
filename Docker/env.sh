0e78a8f39f961e621813032348cf078d88855089
Bruce Campbell <brcampbe@microsoft.com>
2020-10-05 17:02:32 +0000

    remove cruft

--- Docker/env.sh
+++
@@ -1,9 +0,0 @@
-export CC=clang
-export CXX=clang++
-export LD=ld.lld
-export CFLAGS="-Wno-unused-command-line-argument -Wl,-I/lib64/ld-linux-x86-64.so.2"
-export CXXFLAGS="-Wno-unused-command-line-argument -Wl,-I/lib64/ld-linux-x86-64.so.2"
-export LDFLAGS="-I/lib64/ld-linux-x86-64.so.2"
-export C_INCLUDE_PATH="/usr/include:/usr/include/x86_64-linux-gnu"
-export CXX_INCLUDE_PATH="/usr/include:/usr/include/x86_64-linux-gnu"
-
